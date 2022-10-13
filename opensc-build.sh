c6872d989ec5314f83cb37911af0403730553fb6
Mouse <mouse008@gmail.com>
2022-10-13 18:13:29 -0400

    Add build script

---
+++ opensc-build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash +ex
+
+sudo date && make clean && rm -rf target/* && ./bootstrap && MacOSX/build && sudo /usr/local/bin/opensc-uninstall && sudo installer -pkg OpenSC.pkg -target /
