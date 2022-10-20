cf57c8f221e95aeb04532bb8e14db733ad45cd0e
Mouse <mouse008@gmail.com>
2022-10-19 21:48:09 -0400

    Added date output

--- opensc-build.sh
+++ opensc-build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash +ex
 
 sudo date && make clean && rm -rf target/* && ./bootstrap && MacOSX/build && sudo /usr/local/bin/opensc-uninstall && sudo installer -pkg OpenSC.pkg -target /
+date
